dd39e82d076d6e83acc64f1c08c6ea17526f2669
THOMAS SIMON STEINMANN <19591676@students.ltu.edu.au>
2018-04-26 16:44:30 +1000

    Minor cleanup

--- task1.m
+++ task1.m
@@ -7,3 +7,10 @@ imgSizeY = 30;
 dbNormal = buildImagesDB('Training\', 'subject', 'normal.jpg', 10, 'names.txt', imgSizeX, imgSizeY);
 dbHappy= buildImagesDB('Training\', 'subject', 'happy.jpg', 10, 'names.txt', imgSizeX, imgSizeY);
 dbSad = buildImagesDB('Training\', 'subject', 'sad.jpg', 10, 'names.txt', imgSizeX, imgSizeY);
+
+trainDB = [dbNormal, dbHappy, dbSad];
+
+dbSleepy = buildImagesDB('Testing\', 'subject', 'sleepy.jpg', 10, 'names.txt', imgSizeX, imgSizeY);
+dbSurprised = buildImagesDB('Testing\', 'subject', 'surprised.jpg', 10, 'names.txt', imgSizeX, imgSizeY);
+
+testDB = [dbSleepy, dbSurprised];
