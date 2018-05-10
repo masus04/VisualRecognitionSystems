980b2f81b0b3b2b9dbdc2d8afaf83dcd5b462b4c
ROWA ABDULAZIZ M ALMISKEY <18222534@students.ltu.edu.au>
2018-05-10 16:02:36 +1000

    Upload Pictures

--- Assignment1.m
+++ Assignment1.m
@@ -1,7 +1,7 @@
-imgSizeX = 40;
-imgSizeY = 30;
-% imgSizeX = 80;
-% imgSizeY = 60;
+%imgSizeX = 40;
+%imgSizeY = 30;
+imgSizeX = 80;
+imgSizeY = 60;
 
 % || - 1. Building training Model - ||
 % Added X and Y sizes for image rescaling to buildImagesDB
@@ -17,30 +17,30 @@ dbSurprised = buildImagesDB('Testing\', 'subject', 'surprised.jpg', 10, 'names.t
 testDB = [dbSleepy, dbSurprised];
 
 % || - 2. Generate top K eigenfaces - ||
-model85 = buildFacesModel(trainDB, 0.85);
-plotEigenfaces(model85.keigfaces, model85.meanFace, 5, imgSizeX, imgSizeY);
+K1 = buildFacesModel(trainDB, 0.85);
+plotEigenfaces(K1.keigfaces, K1.meanFace, 5, imgSizeX, imgSizeY);
 
-model95 = buildFacesModel(trainDB, 0.95);
-plotEigenfaces(model95.keigfaces(:, 1:10), model95.meanFace, 5, imgSizeX, imgSizeY);
+K2 = buildFacesModel(trainDB, 0.95);
+plotEigenfaces(K2.keigfaces(:, 1:10), K2.meanFace, 5, imgSizeX, imgSizeY);
 
 % || - 3. Recognise Faces using 1,3,5 KNN - ||
 % Also computed MEAN neighbour
-labelledDB = recognizeFacesKnn(testDB, model85, 1);
-plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '1NN, Accuraccy: 0.85');
-labelledDB = recognizeFacesKnn(testDB, model85, 3);
-plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '3NN, Accuraccy: 0.85');
-labelledDB = recognizeFacesKnn(testDB, model85, 5);
-plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '5NN, Accuraccy: 0.85');
-labelledDB = recognizeFacesAvg(testDB, model85, 10, 3);
-plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, 'AVG, Accuraccy: 0.85');
+labelledDB = recognizeFacesKnn(testDB, K1, 1);
+plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '1NN, K1');
+labelledDB = recognizeFacesKnn(testDB, K1, 3);
+plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '3NN,  K1');
+labelledDB = recognizeFacesKnn(testDB, K1, 5);
+plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '5NN,  K1');
+labelledDB = recognizeFacesAvg(testDB, K1, 10, 3);
+plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, 'AVG,  K1');
 
-labelledDB = recognizeFacesKnn(testDB, model95, 1);
-plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '1NN, Accuraccy: 0.95');
-labelledDB = recognizeFacesKnn(testDB, model95, 3);
-plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '3NN, Accuraccy: 0.95');
-labelledDB = recognizeFacesKnn(testDB, model95, 5);
-plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '5NN, Accuraccy: 0.95');
-labelledDB = recognizeFacesAvg(testDB, model95, 10, 3);
-plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, 'AVG, Accuraccy: 0.95');
+labelledDB = recognizeFacesKnn(testDB, K2, 1);
+plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '1NN,  K2');
+labelledDB = recognizeFacesKnn(testDB, K2, 3);
+plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '3NN,  K2');
+labelledDB = recognizeFacesKnn(testDB, K2, 5);
+plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, '5NN,  K2');
+labelledDB = recognizeFacesAvg(testDB, K2, 10, 3);
+plotDBImagesWithLabels(labelledDB, 'grayCrop', 5, 'AVG,  K2');
 
 % || - 4. Recognise Faces using 1,3,5 KNN - ||
