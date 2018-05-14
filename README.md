853f4f00d92d1682af6fd38da26a12ee9444c7ca
THOMAS SIMON STEINMANN <19591676@students.ltu.edu.au>
2018-05-14 12:54:41 +1000

    Removed unused files

--- README.md
+++
@@ -1,8 +0,0 @@
-# VisualRecognitionSystems
-
-1. git pull
-
-2. Make changes
-3. git add .
-4. git commit -m "message"
-5. git push
--- recognizeClosestFaces.m
+++
@@ -1,5 +0,0 @@
-function myTop5imgsDB = recognizeClosestFaces(testDB, trainModel, k)
-    testW = projFaces(obtainDataVects(testDB), trainModel.keigfaces, trainModel.meanFace);
-    predFaces = findClosestKFaces(testW, trainModel.trainW, trainModel.klambda, 'euc', k);
-    myTop5imgsDB = trainModel.trainDB(predFaces(:));
-end
